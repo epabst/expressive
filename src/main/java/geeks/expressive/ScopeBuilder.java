4d42e3106dc95a002d45b88294e0380370c29a5f
Eric Pabst <pabstec@familysearch.org>
2009-12-22 16:44:02 -0700

    Added ScopeBuilder.

---
+++ src/main/java/geeks/expressive/ScopeBuilder.java
@@ -0,0 +1,67 @@
+package geeks.expressive;
+
+import org.reflections.util.ClasspathHelper;
+import static org.reflections.util.DescriptorHelper.qNameToResourceName;
+import org.reflections.util.Utils;
+import org.reflections.util.AbstractConfiguration;
+import org.reflections.util.FilterBuilder;
+import org.reflections.Reflections;
+import org.reflections.scanners.MethodAnnotationsScanner;
+
+import java.net.URL;
+import java.util.*;
+import java.lang.reflect.Method;
+import java.lang.annotation.Annotation;
+
+/**
+ * A builder for a Scope.
+ *
+ * @author pabstec
+ */
+public class ScopeBuilder {
+  private final Map<URL, String> descriptionByUrl = new LinkedHashMap<URL, String>();
+
+  public Scope build() {
+    return asScope(new ArrayList<String>(descriptionByUrl.values()), descriptionByUrl.keySet());
+  }
+
+  public ScopeBuilder with(Class<?> aClass) {
+    String description = aClass.getName();
+    String classResourceName = qNameToResourceName(description) + ".class";
+    return add(Utils.getEffectiveClassLoader().getResource(classResourceName), description);
+  }
+
+  public ScopeBuilder with(Package aPackage) {
+    Collection<URL> urls = ClasspathHelper.getUrlsForPackagePrefix(aPackage.getName());
+    for (URL url : urls) {
+      add(url, aPackage.getName());
+    }
+    return this;
+  }
+
+  private ScopeBuilder add(URL url, String description) {
+    descriptionByUrl.put(url, description);
+    return this;
+  }
+
+  private static Scope asScope(List<String> pathsForDescription, final Collection<URL> urls) {
+    return asScope(pathsForDescription, new Reflections(new AbstractConfiguration() {{
+      setFilter(new FilterBuilder().include(".*"));
+      setUrls(urls);
+      setScanners(new MethodAnnotationsScanner());
+    }}));
+  }
+
+  private static Scope asScope(final List<String> pathsForDescription, final Reflections reflections) {
+    return new Scope() {
+      public Set<Method> getMethodsAnnotatedWith(Class<? extends Annotation> annotationClass) {
+        return reflections.getMethodsAnnotatedWith(annotationClass);
+      }
+
+      @Override
+      public String toString() {
+        return new StringBuilder().append("Scope").append("{paths=").append(pathsForDescription).append('}').toString();
+      }
+    };
+  }
+}
--- src/main/java/geeks/expressive/Scopes.java
+++ src/main/java/geeks/expressive/Scopes.java
@@ -1,21 +1,5 @@
 package geeks.expressive;
 
-import org.reflections.Reflections;
-import org.reflections.scanners.MethodAnnotationsScanner;
-import org.reflections.util.AbstractConfiguration;
-import org.reflections.util.FilterBuilder;
-import org.reflections.util.ClasspathHelper;
-import org.reflections.util.Utils;
-import static org.reflections.util.DescriptorHelper.qNameToResourceName;
-
-import java.util.Arrays;
-import java.util.Set;
-import java.util.List;
-import java.util.Collection;
-import java.lang.reflect.Method;
-import java.lang.annotation.Annotation;
-import java.net.URL;
-
 /**
  * A factory for scopes.
  *
@@ -23,33 +7,10 @@ import java.net.URL;
  */
 public class Scopes {
   public static Scope asScope(final Class<?> aClass) {
-    String classResourceName = qNameToResourceName(aClass.getName()) + ".class";
-    final List<URL> urls = Arrays.asList(Utils.getEffectiveClassLoader().getResource(classResourceName));
-    return asScope(Arrays.asList(aClass.getName()), urls);
+    return new ScopeBuilder().with(aClass).build();
   }
 
   public static Scope asScope(final Package aPackage) {
-    return asScope(Arrays.asList(aPackage.getName()), ClasspathHelper.getUrlsForPackagePrefix(aPackage.getName()));
-  }
-
-  private static Scope asScope(List<String> pathsForDescription, final Collection<URL> urls) {
-    return asScope(pathsForDescription, new Reflections(new AbstractConfiguration() {{
-      setFilter(new FilterBuilder().include(".*"));
-      setUrls(urls);
-      setScanners(new MethodAnnotationsScanner());
-    }}));
-  }
-
-  private static Scope asScope(final List<String> pathsForDescription, final Reflections reflections) {
-    return new Scope() {
-      public Set<Method> getMethodsAnnotatedWith(Class<? extends Annotation> annotationClass) {
-        return reflections.getMethodsAnnotatedWith(annotationClass);
-      }
-
-      @Override
-      public String toString() {
-        return new StringBuilder().append("Scope").append("{paths=").append(pathsForDescription).append('}').toString();
-      }
-    };
+    return new ScopeBuilder().with(aPackage).build();
   }
 }
