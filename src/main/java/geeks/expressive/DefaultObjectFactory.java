b7541e651a1c8e5b34ccdaa8a2f2310e13fc9616
Eric Pabst <pabstec@familysearch.org>
2009-12-26 00:23:06 -0700

    Support auto-registration of component implementations of interfaces..

--- src/main/java/geeks/expressive/DefaultObjectFactory.java
+++ src/main/java/geeks/expressive/DefaultObjectFactory.java
@@ -5,6 +5,10 @@ import org.picocontainer.PicoBuilder;
 import org.picocontainer.PicoContainer;
 import org.picocontainer.monitors.ComposingMonitor;
 
+import java.util.Set;
+import java.util.HashSet;
+import java.lang.reflect.Modifier;
+
 /**
  * A wrapper for picocontainer.
  *
@@ -12,6 +16,8 @@ import org.picocontainer.monitors.ComposingMonitor;
  */
 public class DefaultObjectFactory implements ObjectFactory {
   private final MutablePicoContainer container;
+  private final Set<Class<?>> registeredComponentClasses = new HashSet<Class<?>>();
+  private final Set<Object> addedInstances = new HashSet<Object>();
 
   public DefaultObjectFactory() {
     this.container = new PicoBuilder().withCaching().withConstructorInjection().withMonitor(new ComposingMonitor(new ComposingMonitor.Composer() {
@@ -19,8 +25,22 @@ public class DefaultObjectFactory implements ObjectFactory {
       public Object compose(PicoContainer picoContainer, Object o) {
         if (o instanceof Class) {
           Class<?> componentClass = (Class<?>) o;
-          container.addComponent(componentClass);
-          return getInstance(componentClass);
+          if (registeredComponentClasses.add(componentClass)) {
+            if (Modifier.isAbstract(componentClass.getModifiers())) {
+              for (Object addedInstance : addedInstances) {
+                if (componentClass.isInstance(addedInstance)) {
+                  //auto-register the matching instance since it hasn't been registered yet
+                  container.addComponent(componentClass, addedInstance);
+                }
+              }
+              return getInstance(componentClass);
+            }
+            else {
+              //auto-register the class since it hasn't been registered yet
+              container.addComponent(componentClass);
+              return getInstance(componentClass);
+            }
+          }
         }
         return null;
       }
@@ -36,6 +56,10 @@ public class DefaultObjectFactory implements ObjectFactory {
   }
 
   public void addInstance(Object instance) {
+    if (instance instanceof Class) {
+      throw new IllegalArgumentException("a class is not a component instance: " + instance);
+    }
+    addedInstances.add(instance);
     container.addComponent(instance);
   }
 }
--- src/test/java/geeks/expressive/TestDefaultObjectFactory.java
+++ src/test/java/geeks/expressive/TestDefaultObjectFactory.java
@@ -1,7 +1,8 @@
 package geeks.expressive;
 
 import org.testng.annotations.Test;
-import static org.testng.Assert.assertNotNull;
+import static org.testng.Assert.*;
+import org.picocontainer.PicoCompositionException;
 
 /**
  * A test for {@link DefaultObjectFactory}.
@@ -11,22 +12,64 @@ import static org.testng.Assert.assertNotNull;
 ///CLOVER:OFF
 public class TestDefaultObjectFactory {
   @Test
-  public void testInjectedComponent() {
+  public void shouldSupportUnregisteredInjectedComponent() {
     DefaultObjectFactory objectFactory = new DefaultObjectFactory();
     Component1 instance1 = objectFactory.getInstance(Component1.class);
     assertNotNull(instance1);
     assertNotNull(instance1.component2);
   }
 
+  @Test
+  public void shouldSupportRegisteredComponentInterface() {
+    DefaultObjectFactory objectFactory = new DefaultObjectFactory();
+    objectFactory.addInstance(new Component2());
+    ComponentInterface instance1 = objectFactory.getInstance(ComponentInterface.class);
+    assertEquals(instance1.getClass(), Component2.class);
+  }
+
+  @Test
+  public void shouldFailWithComponentInterfaceRegisteredMultipleTimes() {
+    DefaultObjectFactory objectFactory = new DefaultObjectFactory();
+    objectFactory.addInstance(new Component2());
+    objectFactory.addInstance(new Component3());
+    try {
+      objectFactory.getInstance(ComponentInterface.class);
+      fail("expected exception");
+    }
+    catch (PicoCompositionException e) {
+      assertTrue(e.getMessage().contains("Duplicate"));
+      assertTrue(e.getMessage().contains(ComponentInterface.class.getName()));
+    }
+  }
+
+  @Test
+  public void shouldNotAllowAddingComponentClass() {
+    DefaultObjectFactory objectFactory = new DefaultObjectFactory();
+    try {
+      objectFactory.addInstance(Component1.class);
+      fail("expected exception");
+    }
+    catch (IllegalArgumentException e) {
+      assertTrue(e.getMessage().contains("class"), e.toString());
+    }
+  }
+
   public static class Component1 {
+
     private final Component2 component2;
 
     public Component1(Component2 component2) {
       this.component2 = component2;
     }
+
   }
 
-  public static class Component2 {
+  public static interface ComponentInterface {
+  }
 
+  public static class Component2 implements ComponentInterface {
+  }
+
+  public static class Component3 implements ComponentInterface {
   }
 }
